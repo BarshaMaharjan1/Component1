3b05bff198b3fcd87f86c3f6ca47abd2b18b5dca
Barsha Maharjan <barshamhrzan@gmail.com>
2020-11-14 18:01:25 +0545

    Component 1: Development Environments,Version Control, OOP Assignment files added

---
+++ awe/GamesProduct.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace awe;
+
+
+class GamesProduct extends ShopProduct
+{
+    private $pegi;
+
+    public function __construct(
+        string $id,
+        string $title,
+        string $firstName,
+        string $mainName,
+        float $price,
+        int $pegi
+    )
+    {
+        parent::__construct(
+            $id,
+            $title,
+            $firstName,
+            $mainName,
+            $price
+        );
+        $this->pegi = $pegi;
+    }
+
+    public function getPegiRating()
+    {
+        return $this->pegi;
+    }
+}
